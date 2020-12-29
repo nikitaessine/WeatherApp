95601bf227dbf726282c424aa6e5be06bee746a4
Nikita Essine <54572118+nikitaessine@users.noreply.github.com>
2020-12-29 17:55:46 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import eel
+import pyowm
+
+#API key here
+owm = pyowm.OWM("")
+
+@eel.expose
+def get_weather(place):
+
+
+    
+    mgr = owm.weather_manager()
+
+    try:
+        observation = mgr.weather_at_place(place)
+        w = observation.weather
+    except:
+        return "DID NOT FOUND CITY CALLED" + place + " !"
+
+    temp = w.temperature('celsius')['temp']
+    temp = round(temp)
+
+    if temp == 1:
+        
+        return "Temperature in " + place + " is " + str(temp) + " degree!"
+    else:
+        return "Temperature in " + place + " is " + str(temp) + " degrees!"
+    
+   
+
+eel.init('web')
+eel.start('main.html', size=(700,700))
